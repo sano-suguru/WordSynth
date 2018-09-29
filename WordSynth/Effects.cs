0fcea48a9a95233688b5deea5254860cf22065c1
Sano Suguru <sano@mbp17.local>
2018-09-29 23:06:56 +0900

    フォーマットを修正

--- WordSynth/Effects.cs
+++ WordSynth/Effects.cs
@@ -6,12 +6,8 @@ namespace WordSynth {
     public static Func<string, string> Reverce() => (string words) =>
       string.Join(" ", words.Split(" ").Select(word => string.Concat(word.Reverse())));
 
-    public static Func<string, string> Echo(int rate) =>
-      (string words)
-        => string.Concat(
-          words.Select(c => c.ToString() == " "
-                       ? c.ToString()
-                       : string.Concat(Enumerable.Repeat(c, rate))));
+    public static Func<string, string> Echo(int rate) => (string words) =>
+      string.Concat(words.Select(c => c == ' '  ? c.ToString() : string.Concat(Enumerable.Repeat(c, rate))));
 
     public static Func<string, string> Loud(int level) => (string words) =>
       string.Join(" ", words.Split(" ").Select(word => word.ToUpper() + string.Concat(Enumerable.Repeat("!", level))));
