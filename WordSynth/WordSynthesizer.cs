9c7f0a46b3d148f30b846786f1b969feef5f8338
Sano Suguru <sano@mbp17.local>
2018-09-29 23:20:21 +0900

    WordSynthesizerを作成

--- WordSynth/WordSynthesizer.cs
+++ WordSynth/WordSynthesizer.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace WordSynth {
   public class WordSynthesizer {
-    public WordSynthesizer() {
-    }
+    IList<Func<string, string>> Effects { get; }
+
+    public WordSynthesizer() =>
+      Effects = new List<Func<string, string>>();
+
+    public void AddEffect(Func<string, string> effect) => Effects.Add(effect);
+
+    public string Play(string originalWords) =>
+      Effects.Aggregate(originalWords, (words, effect) => effect.Invoke(words));
   }
 }
