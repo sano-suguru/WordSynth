dc29e15e207cfdbfc368a30e8b975b5c68027935
Sano Suguru <sano@mbp17.local>
2018-09-29 20:49:28 +0900

    Effects.Echoのテストを追加

--- WordSynthTest/EffectsTest.cs
+++ WordSynthTest/EffectsTest.cs
@@ -9,5 +9,11 @@ namespace WordSynthTest {
       Func<string, string> effect = Effects.Reverce;
       Assert.Equal("#C si lufrednow", effect.Invoke("C# is wonderful"));
     }
+
+    [Fact]
+    public void TestEcho() {
+      Func<string, string> effect = Effects.Echo(2);
+      Assert.Equal("CC## iiss wwoonnddeerrffuull!!", effect.Invoke("C# is wonderful!"));
+    }
   }
 }
